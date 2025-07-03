f4f587884ffa033bd1f69d69a9fa91b409f935b2
bg4abs <bg4abs@163.com>
2025-07-04 00:06:53 +0800

    Update consts.ts

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -2,5 +2,5 @@ export const GITHUB_URL_VERT = "https://github.com/VERT-sh/VERT";
 export const GITHUB_URL_VERTD = "https://github.com/VERT-sh/vertd";
 export const GITHUB_API_URL = "https://api.github.com/repos/VERT-sh/VERT";
 export const DISCORD_URL = "https://discord.gg/kqevGxYPak";
-export const VERT_NAME = "My Converter";
+export const VERT_NAME = "My converter- The Privacy-first converter";
 export const CONTACT_EMAIL = "hello@vert.sh";
